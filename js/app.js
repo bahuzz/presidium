79e3f9d4bf1ef0b83ec9cb77019c37901d8d750e
bahuzz <bahuzz@mail.ru>
2016-04-13 19:25:06 +0300

    global preloader

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ $(document).ready(function(){
     $('.sidebar').slideToggle();
   });
 
+  $('.btn-preloader').click(function(){
+    $('.global-preloader').fadeIn();
+  });
+
   $('.search-adv').click(function(){
     $('.search-form-adv').slideToggle();
   });
