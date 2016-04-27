ae5d9b9bae79a9847e26dcd0bdebaec241e5b5d7
Dmitry Pochodnya <dmitriy.troy@gmail.com>
2016-04-27 13:45:26 +0300

    Add file upload for mail

--- js/app.js
+++ js/app.js
@@ -471,6 +471,27 @@ if($('.mail-wrapper').length) {
       scrollTop: $('form:last-child').offset().top
     }, 1000);
   })
+
+  $( '.inputfile' ).each( function() {
+    var $input   = $( this ),
+      $label   = $input.next( 'label' ),
+      labelVal = $label.html();
+
+    $input.on( 'change', function( e ) {
+      var fileName = '';
+
+      if( this.files && this.files.length > 1 )
+        fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+      else if( e.target.value )
+        fileName = e.target.value.split( '\\' ).pop();
+
+      if( fileName )
+        $label.find( 'span' ).html( fileName );
+      else
+        $label.html( labelVal );
+    });
+  });
+  
   tinymce.init({ selector:'.mail-wrapper textarea' });
 }
 
