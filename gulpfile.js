3c4326527e687ab63a31f4dc9bac1fe5569a14dd
Dmitry Pochodnya <dmitriy.troy@gmail.com>
2016-05-11 16:36:35 +0300

    New mail

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-var gulp 		 = require('gulp'),
-	compass 	 = require('gulp-compass'),
+var gulp 		   = require('gulp'),
+	compass 	 	 = require('gulp-compass'),
 	autoprefixer = require('gulp-autoprefixer'),
 	browserSync  = require('browser-sync').create(),
+	panini       = require('panini'),
 	imagemin     = require('imagemin');
 
 
@@ -18,6 +19,25 @@ gulp.task('compass', function() {
 	.pipe(browserSync.stream());
 });
 
+// Copy page templates into finished HTML files
+gulp.task('pages', function() {
+  gulp.src('views/pages/**/*.{html,hbs,handlebars}')
+    .pipe(panini({
+      root: 'views/pages/',
+      layouts: 'views/layouts/',
+      partials: 'views/partials/',
+      data: 'views/data/',
+      helpers: 'views/helpers/'
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('pages:reset', function(cb) {
+  panini.refresh();
+  gulp.run('pages');
+  cb();
+});
+
 //Browser sync
 gulp.task('browserSync', function() {
 	browserSync.init({
@@ -30,8 +50,10 @@ gulp.task('browserSync', function() {
 // Watch Task
 gulp.task('watch', function() {
 	gulp.watch('sass/*', ['compass']);
-	gulp.watch("./*.html").on('change', browserSync.reload);
+	gulp.watch(['views/pages/**/*.html'], ['pages', browserSync.reload]);
+  gulp.watch(['views/{layouts,partials}/**/*.html'], ['pages:reset', browserSync.reload]);
+	// gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 // Default Task
-gulp.task('default', ['compass', 'browserSync', 'watch']);
+gulp.task('default', ['compass', 'pages', 'browserSync', 'watch']);
